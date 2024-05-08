ed2cf88894957468e554eff2d53d235ea8624935
Roy <lyt2980999208@gmail.com>
2024-05-08 11:00:25 +0800

    feat: 图片超时异常处理, close #26

--- nonebot-plugin-csgo-case-simulator/utils.py
+++ nonebot-plugin-csgo-case-simulator/utils.py
@@ -10,49 +10,28 @@ import httpx
 from .model import SelectedSkin
 
 
-FONT_DIR = dirname(__file__) + "/font/NotoSansSC-Bold.otf"
-PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = dirname(__file__)
+ASSSETS_DIR = PATH + "/assets"
+FONT_DIR = PATH + "/font/NotoSansSC-Bold.otf"
 
 
 class Utils:
     def __init__(self):
         self.client = httpx.Client()
         self.rarity_color = {
-            "消费级": {
-                "bg": "#5D6884",
-                "stroke": "#4C556D"
-            },
-            "工业级": {
-                "bg": "#3869E8",
-                "stroke": "#2E5AC9"
-            },
-            "军规级": {
-                "bg": "#3B38E8",
-                "stroke": "#2827A7"
-            },
-            "受限": {
-                "bg": "#9033D9",
-                "stroke": "#6C26A7"
-            },
-            "保密": {
-                "bg": "#DE55EA",
-                "stroke": "#A842B3"
-            },
-            "隐秘": {
-                "bg": "#8D1F3B",
-                "stroke": "#6C1531"
-            },
-            "违禁": {
-                "bg": "#C7B61F",
-                "stroke": "#AD9F1E"
-            },
-            "非凡": {
-                "bg": "#C7B61F",
-                "stroke": "#AD9F1E"
-            },
+            "消费级": {"bg": "#5D6884", "stroke": "#4C556D"},
+            "工业级": {"bg": "#3869E8", "stroke": "#2E5AC9"},
+            "军规级": {"bg": "#3B38E8", "stroke": "#2827A7"},
+            "受限": {"bg": "#9033D9", "stroke": "#6C26A7"},
+            "保密": {"bg": "#DE55EA", "stroke": "#A842B3"},
+            "隐秘": {"bg": "#8D1F3B", "stroke": "#6C1531"},
+            "违禁": {"bg": "#C7B61F", "stroke": "#AD9F1E"},
+            "非凡": {"bg": "#C7B61F", "stroke": "#AD9F1E"},
         }
 
-    async def merge_images(self, items: List[SelectedSkin], case_name: str, case_img: str, user_name: str):
+    async def merge_images(
+        self, items: List[SelectedSkin], case_name: str, case_img: str, user_name: str
+    ):
         image_list = []
 
         image_tasks = [self.download_image(item.image) for item in items]
@@ -67,12 +46,12 @@ class Utils:
         main_img.paste(case_img, (128, 505), case_img)
 
         statistic_dict = {}
-        rare_sorted = sorted([item.rarity
-                             for item in items], key=self.rare_sorted_func)
+        rare_sorted = sorted([item.rarity for item in items], key=self.rare_sorted_func)
         for i in range(len(rare_sorted)):
             statistic_dict[rare_sorted[i]] = rare_sorted.count(rare_sorted[i])
-        sorted_counts = sorted(statistic_dict.keys(),
-                               key=self.rare_sorted_func, reverse=True)
+        sorted_counts = sorted(
+            statistic_dict.keys(), key=self.rare_sorted_func, reverse=True
+        )
 
         top_three = {}
         for i in range(len(sorted_counts) if len(sorted_counts) < 3 else 3):
@@ -85,8 +64,9 @@ class Utils:
     def generate_main_img(self, skins: List[Image.Image], items: List[SelectedSkin]):
         for i in range(len(skins)):
             skins[i] = self.generate_item_card(
-                skins[i], items[i].rarity, items[i].name, items[i].wear)
-        main = Image.open(os.path.join(PATH, "main_template.png"))
+                skins[i], items[i].rarity, items[i].name, items[i].wear
+            )
+        main = Image.open(ASSSETS_DIR + "/main_template.png")
         columns = 5
         rows = (len(skins) - 1) // columns + 1
         if rows == 1:
@@ -94,8 +74,9 @@ class Utils:
         for i in range(rows):
             for j in range(columns):
                 index = i * columns + j
-                main.paste(skins[index], (455 + j *
-                           164, 101 + 151 * i), skins[i * columns + j])
+                main.paste(
+                    skins[index], (455 + j * 164, 101 + 151 * i), skins[i * columns + j]
+                )
         return main
 
     def generate_statistic(self, main_img: Image.Image, top_three: dict):
@@ -104,51 +85,58 @@ class Utils:
         i = 0
         for rare, count in top_three.items():
             main_draw.rounded_rectangle(
-                (25, 246 + 80 * i, 405, 313 + 80 * i), radius=2, fill=self.rarity_color[rare]["bg"])
+                (25, 246 + 80 * i, 405, 313 + 80 * i),
+                radius=2,
+                fill=self.rarity_color[rare]["bg"],
+            )
+            main_draw.text((35, 255 + 80 * i), rare, font=info_font, fill="#FFFFFF")
             main_draw.text(
-                (35, 255 + 80 * i), rare, font=info_font, fill="#FFFFFF")
-            main_draw.text((345, 255 + 80 * i), f"x{count}",
-                           font=info_font, fill="#FFFFFF")
+                (345, 255 + 80 * i), f"x{count}", font=info_font, fill="#FFFFFF"
+            )
             i += 1
 
     def generate_info(self, main_img: Image.Image, case_name: str, user_name: str):
         info_font = ImageFont.truetype(FONT_DIR, 32)
         text_width, text_height = info_font.getsize(user_name[:12])
-        rank_img = Image.open(os.path.join(PATH, "rank.png")).resize(
-            (71, 43), Image.LANCZOS)
-        main_img.paste(
-            rank_img, ((500 - text_width) // 2 - 75, 160), rank_img)
+        rank_img = Image.open(ASSSETS_DIR + "/rank.png").resize((71, 43), Image.LANCZOS)
+        main_img.paste(rank_img, ((500 - text_width) // 2 - 75, 160), rank_img)
 
         main_draw = ImageDraw.Draw(main_img)
-        main_draw.text(((500 - text_width) // 2, 155),
-                       user_name[:12], font=info_font, fill="white")
+        main_draw.text(
+            ((500 - text_width) // 2, 155), user_name[:12], font=info_font, fill="white"
+        )
 
         text_width, text_height = info_font.getsize(case_name[:12])
         main_draw.text(
-            ((430 - text_width) // 2, 645), case_name[:12], font=info_font, fill="white")
+            ((430 - text_width) // 2, 645), case_name[:12], font=info_font, fill="white"
+        )
 
         return main_img
 
-    def generate_item_card(self, skin: Image.Image, rarity: str, name: str, wear: Union[str, None]):
+    def generate_item_card(
+        self, skin: Image.Image, rarity: str, name: str, wear: Union[str, None]
+    ):
         image = Image.new("RGBA", (500, 500), "#D4D2D5")
         image = self.generate_item_card_band(
-            image, self.rarity_color[rarity]["bg"], self.rarity_color[rarity]["stroke"])
-        background_img = Image.open(os.path.join(
-            PATH, "bg.png")).resize((500, 379), Image.LANCZOS)
+            image, self.rarity_color[rarity]["bg"], self.rarity_color[rarity]["stroke"]
+        )
+        background_img = Image.open(ASSSETS_DIR + "/bg.png").resize(
+            (500, 379), Image.LANCZOS
+        )
         image.paste(background_img, (0, 0), background_img)
         skin = skin.resize((500, 394), Image.LANCZOS)
         image.paste(skin, (0, 0), skin)
         draw = ImageDraw.Draw(image)
         font = ImageFont.truetype(FONT_DIR, 35)
-        draw.text((20, 390), "\n".join(name.split(" | ")),
-                  font=font, fill='white')
-        if (wear != None):
-            draw.text((480 - font.getsize(wear)
-                      [0], 435), wear, font=font, fill='white')
+        draw.text((20, 390), "\n".join(name.split(" | ")), font=font, fill="white")
+        if wear != None:
+            draw.text((480 - font.getsize(wear)[0], 435), wear, font=font, fill="white")
         image = image.resize((128, 128), Image.LANCZOS)
         return image
 
-    def generate_item_card_band(self, image: Image.Image, color: str, stoke_color) -> Image.Image:
+    def generate_item_card_band(
+        self, image: Image.Image, color: str, stoke_color
+    ) -> Image.Image:
         band_bg = Image.new("RGBA", (500, 121), color)
         image.paste(band_bg, (0, 379), band_bg)
         band = Image.new("RGBA", (300, 20), stoke_color)
@@ -158,10 +146,13 @@ class Utils:
         return image
 
     async def download_image(self, url) -> Image.Image:
-        async with httpx.AsyncClient(verify=False) as client:
-            response = await client.get(url, timeout=10)
-            img = Image.open(BytesIO(response.content))
-            return img
+        try:
+            async with httpx.AsyncClient(verify=False) as client:
+                response = await client.get(url, timeout=None)
+                img = Image.open(BytesIO(response.content))
+                return img
+        except Exception as e:
+            return Image.open(ASSSETS_DIR + "/error.png")
 
     def img_from_PIL(self, pic: Image.Image) -> str:
         buf = BytesIO()
@@ -170,7 +161,7 @@ class Utils:
 
     async def img_from_url(self, url: str) -> str:
         async with httpx.AsyncClient(verify=False) as client:
-            response = await client.get(url, timeout=10)
+            response = await client.get(url, timeout=None)
             return response.content
 
     def generate_case_list_img(self, case_list: list):
@@ -202,9 +193,18 @@ class Utils:
             y = (i // 2) * line_height + 15
 
             # 绘制文本
-            draw.text((x, y), '•' + item, font=font, fill="#272829")
+            draw.text((x, y), "•" + item, font=font, fill="#272829")
         return self.img_from_PIL(image)
 
     def rare_sorted_func(self, x):
-        order = ["消费级", "工业级", "军规级", "受限", "保密", "隐秘", "及其罕见的特殊物品", "非凡"]
+        order = [
+            "消费级",
+            "工业级",
+            "军规级",
+            "受限",
+            "保密",
+            "隐秘",
+            "及其罕见的特殊物品",
+            "非凡",
+        ]
         return order.index(x)
